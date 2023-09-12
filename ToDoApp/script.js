067e2e1922897bbe266b0453f35b0d8cdff6f98f
MVSSS <moskalenko_vldmr@outlook.com>
2023-09-12 16:08:52 +0300

    feat: end work

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -5,9 +5,33 @@ function addTask(){
     if(inputBox.value === ''){
         alert("Please, write something");
     }
-    else{
-        let li = document.createElement("li");
+    else {
+        let li = document.createElement("LI");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("SPAN")
+        span.innerHTML= "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classlist.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+saveData();
