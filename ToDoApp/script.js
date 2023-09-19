388f4cb19de44f5e05374cd094e8bb24d8e4ee9d
MVSSS <moskalenko_vldmr@outlook.com>
2023-09-19 11:59:05 +0300

    fix: fix checked status in tasks, and save results

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -19,10 +19,11 @@ function addTask(){
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classlist.toggle("checked");
+        e.target.classList.toggle("checked");
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
 
@@ -34,4 +35,3 @@ function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
-saveData();
