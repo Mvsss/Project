ca08544adcb0478522111a89ec374ba4f7d54519
MVSSS <moskalenko_vldmr@outlook.com>
2023-09-19 11:32:08 +0300

    feat: end Drag&Drop for image

--- Drag&Drop/script.js
+++ Drag&Drop/script.js
@@ -1,3 +1,4 @@
+// Drag&Drop default //
 let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
@@ -23,3 +24,28 @@ for(list of lists){
             selected = null;
         });
 })};
+// Drag&Drop image //
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imgView = document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage(){
+    inputFile.files[0]
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imgView.style.backgroundImage  = `url(${imgLink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
+}
+
+dropArea.addEventListener("dragover", function(e){
+    e.preventDefault();
+
+});
+
+dropArea.addEventListener("drop", function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});
